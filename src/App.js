8cbe666811bca53535776ea9525c8e408f00ec1b
biratdatta <birat.datta26@gmail.com>
2022-07-26 22:47:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+ import React {useState} from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 function App() {
+
+const url =  'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid='
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
     </div>
   );
 }
