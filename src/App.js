fc6195f33e38e44cb31adb27d7f846645e636219
biratdatta <birat.datta26@gmail.com>
2022-07-28 10:40:49 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
- import React {useState} from 'react'
+ import React, {useState} from 'react'
 import axios from 'axios'
-import { useState } from 'react'
+ 
 function App() {
 
 
@@ -11,15 +11,34 @@ function App() {
     <div className="app">
       <div className="container">
        <div className="top">    
+        <div className="location">
+        <p>Agartala</p> 
+        </div>
+    <div className="temp">
+      <h1>60°F</h1>
+    </div>
+     <div className="description">
+      <p>Clouds</p>
+     </div>
           </div>
-<div className="bottom"></div>
+<div className="bottom">
+  <div className="feels">
+    <p>
+      65°F
+    </p>
+  </div>
+  <div className="humidity">
+<p>20%</p>
+
+  </div>
+  <
+</div>
       </div>
 
  
     </div>
   );
 }
-
 
 
 
